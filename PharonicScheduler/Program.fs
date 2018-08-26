e45e0b448e681c16dac3d5969ad8c123e9b980fa
Nathanial Hartman <nathanial.hartman@gmail.com>
2018-08-25 21:55:59 -0700

    renamed

--- PharonicScheduler/Program.fs
+++ PharonicScheduler/Program.fs
@@ -13,11 +13,21 @@ type SkiaForm() as form =
     let skiaView = new SkiaSharp.Views.Desktop.SKControl()
     let root: Render.Node = Render.Panel((0.0f,0.0f), (100.0f,100.0f),
         [|
+            Render.Shape((75.0f, 75.0f), Render.fillBlackNoBorder, Render.Rectangle((1.0f,1.0f)));
             Render.Shape((10.0f, 10.0f), Render.fillBlackNoBorder, Render.Rectangle((20.f, 20.f)));
             Render.Shape((30.0f, 30.0f), Render.fillBlackNoBorder, Render.Rectangle((30.f, 30.f)));
         |])
 
+        
+    let renderDebugText(canvas: SKCanvas) = 
+        let skPaint = new SKPaint()
+        skPaint.IsAntialias <- true
+        skPaint.HintingLevel <- SKPaintHinting.Full
+        skPaint.Color <- SKColors.Black
+        canvas.DrawText("SkiaSharp", new SKPoint(100.f,100.f), skPaint)
+
     do form.InitializeForm()
+
 
     member this.InitializeForm () =
         skiaView.Dock <- DockStyle.Fill
@@ -35,6 +45,7 @@ type SkiaForm() as form =
     member this.PaintSurface (args: SKPaintSurfaceEventArgs): unit = 
         let canvas = args.Surface.Canvas
         canvas.Clear(SKColors.White)
+        renderDebugText(canvas)
         Render.paint(canvas, root)
 
 
